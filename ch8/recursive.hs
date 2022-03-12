3d7aedf967fed348511a8be04132513bf2430b70
Matt Willy <matt.willy@gmail.com>
2022-03-11 21:57:39 -0700

    different types of recursive functions

---
+++ ch8/recursive.hs
@@ -0,0 +1,16 @@
+module Recursive where
+
+inc :: Num a => a -> a
+inc = (+ 1)
+
+three = inc . inc . inc $ 0
+
+three' = (inc . inc . inc) 0
+
+incTimes :: (Eq a, Num a) => a -> a -> a
+incTimes 0 n = n
+incTimes times n = 1 + incTimes (times - 1) n
+
+applyTimes :: (Eq a, Num a) => a -> (b -> b) -> b -> b
+applyTimes 0 f b = b
+applyTimes n f b = f (applyTimes (n - 1) f b)
