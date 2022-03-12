6b11586160505ae1b30846a0221459eb8d768754
Matt Willy <matt.willy@gmail.com>
2022-03-11 22:20:52 -0700

    add some more recursive functions

--- ch8/recursive.hs
+++ ch8/recursive.hs
@@ -14,3 +14,19 @@ incTimes times n = 1 + incTimes (times - 1) n
 applyTimes :: (Eq a, Num a) => a -> (b -> b) -> b -> b
 applyTimes 0 f b = b
 applyTimes n f b = f (applyTimes (n - 1) f b)
+
+f :: Bool -> Maybe Int
+f False = Just 0
+f _ = Nothing
+
+fibonacci :: Integral a => a -> a
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+  where
+    go n d count
+      | n < d = (count, n)
+      | otherwise = go (n - d) d (count + 1)
