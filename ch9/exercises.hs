ae9d00c3be6ab333daab96adaa08b12edf49e52a
Matt Willy <matt.willy@gmail.com>
2022-05-13 16:17:21 -0600

    add some exercises for lists

--- ch9/exercises.hs
+++ ch9/exercises.hs
@@ -1,5 +1,7 @@
 module Exercises where
 
+import Data.Char
+
 myWords :: String -> [String]
 myWords "" = []
 myWords xs =
@@ -14,3 +16,29 @@ myWords xs =
 
 myFilter :: String -> [String]
 myFilter xs = filter (\x -> x `notElem` ["the", "a", "an"]) $ words xs
+
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _ = []
+myZip _ [] = []
+myZip (x:xs) (y:ys) = (x, y) : myZip xs ys
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _ [] _ = []
+myZipWith _ _ [] = []
+myZipWith f (x:xs) (y:ys) = f x y : myZipWith f xs ys
+
+myZip' :: [a] -> [b] -> [(a, b)]
+myZip' = myZipWith (,)
+
+filterUpper :: [Char] -> [Char]
+filterUpper = filter isUpper
+
+capitalize :: [Char] -> [Char]
+capitalize [] = ""
+capitalize (x:xs) = toUpper x : xs
+
+allCaps :: [Char] -> [Char]
+allCaps = map toUpper
+
+capLetter :: [Char] -> Char
+capLetter = toUpper . head
