944556cb3afcc0fb9f2d018a7ff4f23367f05108
Matt Willy <matt.willy@gmail.com>
2022-05-21 10:02:28 -0600

    complete the squish and squishMap

--- ch9/exercises.hs
+++ ch9/exercises.hs
@@ -42,3 +42,65 @@ allCaps = map toUpper
 
 capLetter :: [Char] -> Char
 capLetter = toUpper . head
+
+myAnd :: [Bool] -> Bool
+myAnd [] = True
+myAnd (x:xs)
+  | x = myAnd xs
+  | otherwise = False
+
+myOr :: [Bool] -> Bool
+myOr [] = False
+myOr (x:xs)
+  | x = True
+  | otherwise = myOr xs
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f xs = myOr $ map f xs
+
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll f xs = myAnd $ map f xs
+
+myElem :: Eq a => a -> [a] -> Bool
+myElem _ [] = False
+myElem val (x:xs)
+  | val == x = True
+  | otherwise = myElem val xs
+
+myElem' :: Eq a => a -> [a] -> Bool
+myElem' val = myAny (== val)
+
+myReverse :: [Char] -> [Char]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+squish :: [[a]] -> [a]
+squish [] = []
+squish (x:xs) =
+  let squishHelper [] [] acc = reverse acc
+      squishHelper [] (x:xs) acc = squishHelper x xs acc
+      squishHelper (x:xs) ys acc = squishHelper xs ys (x : acc)
+   in squishHelper x xs []
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap _ [] = []
+myMap f (x:xs) = f x : myMap f xs
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap _ [] = []
+squishMap f xs = squish $ myMap f xs
+
+squishAgain :: [[a]] -> [a]
+squishAgain = undefined
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy = undefined
+
+myMinimumBy :: (a -> a -> Ordering) -> [a] -> a
+myMinimumBy = undefined
+
+myMaximum :: (Ord a) => [a] -> a
+myMaximum = undefined
+
+myMinimum :: (Ord a) => [a] -> a
+myMinimum = undefined
