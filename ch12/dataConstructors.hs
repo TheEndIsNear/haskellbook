597fff191593adb3925aebee58e59a3a5f406197
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2024-11-29 19:45:32 -0700

    add a function that will return all the errors

--- ch12/dataConstructors.hs
+++ ch12/dataConstructors.hs
@@ -8,7 +8,42 @@ data Person =
   Person Name Age
   deriving (Show)
 
+data PersonInvalid
+  = NameEmpty
+  | AgeTooLow
+  deriving (Eq, Show)
+
+type ValidatePerson a = Either [PersonInvalid] a
+
 mkPerson :: Name -> Age -> Maybe Person
 mkPerson name age
   | name /= "" && age >= 0 = Just $ Person name age
   | otherwise = Nothing
+
+mkPerson' :: Name -> Age -> Either PersonInvalid Person
+mkPerson' name age
+  | name /= "" && age >= 0 = Right $ Person name age
+  | name == "" = Left NameEmpty
+  | otherwise = Left AgeTooLow
+
+ageOkay :: Age -> Either [PersonInvalid] Age
+ageOkay age =
+  case age >= 0 of
+    True -> Right age
+    False -> Left [AgeTooLow]
+
+nameOkay :: Name -> Either [PersonInvalid] Name
+nameOkay name =
+  case name /= "" of
+    True -> Right name
+    False -> Left [NameEmpty]
+
+mkPerson'' :: Name -> Age -> ValidatePerson Person
+mkPerson'' name age = mPerson (nameOkay name) (ageOkay age)
+  where
+    mPerson ::
+         ValidatePerson Name -> ValidatePerson Age -> ValidatePerson Person
+    mPerson (Right nameOk) (Right ageOk) = Right (Person nameOk ageOk)
+    mPerson (Left badName) (Left badAge) = Left (badName ++ badAge)
+    mPerson (Left badName) _ = Left badName
+    mPerson _ (Left badAge) = Left badAge
