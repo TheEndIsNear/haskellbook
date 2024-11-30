8b140971222fc1a656769a0d6485c48c1b220b76
TheEndIsNear <2172212+TheEndIsNear@users.noreply.github.com>
2024-11-29 20:03:15 -0700

    use hlint to clean up some code

--- ch12/dataConstructors.hs
+++ ch12/dataConstructors.hs
@@ -28,15 +28,11 @@ mkPerson' name age
 
 ageOkay :: Age -> Either [PersonInvalid] Age
 ageOkay age =
-  case age >= 0 of
-    True -> Right age
-    False -> Left [AgeTooLow]
+  if age >= 0 then Right age else Left [AgeTooLow]
 
 nameOkay :: Name -> Either [PersonInvalid] Name
 nameOkay name =
-  case name /= "" of
-    True -> Right name
-    False -> Left [NameEmpty]
+  if name /= "" then Right name else Left [NameEmpty]
 
 mkPerson'' :: Name -> Age -> ValidatePerson Person
 mkPerson'' name age = mPerson (nameOkay name) (ageOkay age)
